03cdc8f1aae7bb2670ff087b422d01d663746c5a
Andhika <andhika.ardhiansyah19@gmail.com>
2022-11-07 00:54:30 +0700

    server

---
+++ handlers/users.go
@@ -0,0 +1,151 @@
+package handlers
+
+import (
+	dto "dumbmerch/dto/result"
+	usersdto "dumbmerch/dto/users"
+	"dumbmerch/models"
+	"dumbmerch/repositories"
+	"encoding/json"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+)
+
+type handler struct {
+	UserRepository repositories.UserRepository
+}
+
+func HandlerUser(UserRepository repositories.UserRepository) *handler {
+	return &handler{UserRepository}
+}
+
+func (h *handler) FindUsers(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	users, err := h.UserRepository.FindUsers()
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		response := dto.ErrorResult{Code: http.StatusBadRequest, Message: err.Error()}
+		json.NewEncoder(w).Encode(response)
+	}
+	for i, p := range users {
+		users[i].Image = path_file + p.Image
+	}
+
+	w.WriteHeader(http.StatusOK)
+	response := dto.SuccessResult{Code: "Success", Data: users}
+	json.NewEncoder(w).Encode(response)
+}
+
+func (h *handler) GetUser(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	id, _ := strconv.Atoi(mux.Vars(r)["id"])
+
+	user, err := h.UserRepository.GetUser(id)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		response := dto.ErrorResult{Code: http.StatusBadRequest, Message: err.Error()}
+		json.NewEncoder(w).Encode(response)
+		return
+	}
+	user.Image = path_file + user.Image
+
+	w.WriteHeader(http.StatusOK)
+	response := dto.SuccessResult{Code: "Success", Data: user}
+	json.NewEncoder(w).Encode(response)
+}
+
+func (h *handler) UpdateUser(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	dataContex := r.Context().Value("dataFile") // add this code
+	filename := ""
+	if dataContex != nil {
+		filename = dataContex.(string)
+	}
+
+	request := usersdto.UpdateUserRequest{
+		Name:     r.FormValue("name"),
+		Email:    r.FormValue("email"),
+		Phone:    r.FormValue("phone"),
+		Location: r.FormValue("location"),
+		Role:     r.FormValue("role"),
+	}
+
+	id, _ := strconv.Atoi(mux.Vars(r)["id"])
+
+	user := models.User{}
+
+	if request.Name != "" {
+		user.Name = request.Name
+	}
+
+	if request.Email != "" {
+		user.Email = request.Email
+	}
+
+	if request.Phone != "" {
+		user.Phone = request.Phone
+	}
+	if request.Location != "" {
+		user.Location = request.Location
+	}
+	if filename != "" {
+		user.Image = filename
+	}
+
+	if request.Role != "" {
+		user.Role = request.Role
+	}
+
+	data, err := h.UserRepository.UpdateUser(user, id)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		response := dto.ErrorResult{Code: http.StatusInternalServerError, Message: err.Error()}
+		json.NewEncoder(w).Encode(response)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	response := dto.SuccessResult{Code: "Success", Data: convertResponse(data)}
+	json.NewEncoder(w).Encode(response)
+}
+
+func (h *handler) DeleteUser(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	id, _ := strconv.Atoi(mux.Vars(r)["id"])
+
+	user, err := h.UserRepository.GetUser(id)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		response := dto.ErrorResult{Code: http.StatusBadRequest, Message: err.Error()}
+		json.NewEncoder(w).Encode(response)
+		return
+	}
+
+	data, err := h.UserRepository.DeleteUser(user, id)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		response := dto.ErrorResult{Code: http.StatusInternalServerError, Message: err.Error()}
+		json.NewEncoder(w).Encode(response)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	response := dto.SuccessResult{Code: http.StatusText(http.StatusOK), Data: convertResponse(data)}
+	json.NewEncoder(w).Encode(response)
+}
+
+func convertResponse(u models.User) usersdto.UserResponse {
+	return usersdto.UserResponse{
+		ID:       u.ID,
+		Name:     u.Name,
+		Email:    u.Email,
+		Password: u.Password,
+		Phone:    u.Phone,
+		Location: u.Location,
+	}
+}
