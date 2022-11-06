a080dda459c549452c60a489284bb2be586718ca
Andhika <andhika.ardhiansyah19@gmail.com>
2022-11-07 01:51:10 +0700

    update

--- pkg/mysql/mysql.go
+++ pkg/mysql/mysql.go
@@ -10,14 +10,13 @@ import (
 
 var DB *gorm.DB
 
-var DB_HOST = os.Getenv("DB_HOST")
-var DB_USER = os.Getenv("DB_USER")
-var DB_PASSWORD = os.Getenv("DB_PASSWORD")
-var DB_NAME = os.Getenv("DB_NAME")
-var DB_PORT = os.Getenv("DB_PORT")
-
 // Connection Database
 func DatabaseInit() {
+	var DB_HOST = os.Getenv("DB_HOST")
+	var DB_USER = os.Getenv("DB_USER")
+	var DB_PASSWORD = os.Getenv("DB_PASSWORD")
+	var DB_NAME = os.Getenv("DB_NAME")
+	var DB_PORT = os.Getenv("DB_PORT")
 	var err error
 	// dsn := "root:@tcp(localhost:3306)/waysfood?charset=utf8mb4&parseTime=True&loc=Local"
 	// DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
